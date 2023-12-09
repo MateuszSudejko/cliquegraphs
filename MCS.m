b8bc2e3aa675f03970475c98f0e59a0c04e3ecd3
Piotr Możeluk <122280229+ananasek727@users.noreply.github.com>
2023-12-09 19:57:02 +0100

    Update MCS.m
    now working for multiple graphs

--- MCS.m
+++ MCS.m
@@ -1,28 +1,38 @@
-function output = MCS(graph1, graph2)
-    %   graph1, graph2: Adjacency matrices of the input graphs
-    [modular, index_mapping] = modular_product(graph1, graph2);
-    all_cliques = BronKerboschDirected2(modular);
-    max_size = 0;
-    for i = 1:length(all_cliques)
-        if size(all_cliques{i}) > max_size
-            max_size = length(all_cliques{i});
-            idx = i;
-        end
+function output = MCS(number_of_graphs,graphs)
+    %   graphs: call array with adjacency matrices of the input graphs
+    if number_of_graphs == 0 || number_of_graphs == 1
+        return
     end
-    max_clique = all_cliques{idx};
-    index_mapping_graph1 = [];
-    for i = 1:length(max_clique)
-        index_mapping_graph1 = [index_mapping_graph1, index_mapping{max_clique(i)}(1)];
-    end
-    output = zeros(size(graph1,1));
-    for i = 1:size(graph1,1)
-        for j = 1:size(graph1)
-            if ismember(i, index_mapping_graph1) && ismember(j, index_mapping_graph1)
-                output(i,j) = graph1(i,j);
+    graph1 = graphs{1};
+    graph2 = graphs{2};
+    for k = 2:(number_of_graphs)
+        [modular, index_mapping] = modular_product(graph1, graph2);
+        all_cliques = BronKerboschDirected2(modular);
+        max_size = 0;
+        for i = 1:length(all_cliques)
+            if size(all_cliques{i}) > max_size
+                max_size = length(all_cliques{i});
+                idx = i;
             end
         end
+        max_clique = all_cliques{idx};
+        index_mapping_graph1 = [];
+        for i = 1:length(max_clique)
+            index_mapping_graph1 = [index_mapping_graph1, index_mapping{max_clique(i)}(1)];
+        end
+        output = zeros(size(graph1,1));
+        for i = 1:size(graph1,1)
+            for j = 1:size(graph1)
+                if ismember(i, index_mapping_graph1) && ismember(j, index_mapping_graph1)
+                    output(i,j) = graph1(i,j);
+                end
+            end
+        end
+        isolatedVertices = sum(output, 1) == 0 & sum(output, 2)' == 0;
+        output = output(~isolatedVertices, ~isolatedVertices);
+        graph1 = output;
+        if k+1 <= number_of_graphs
+            graph2 = graphs{k+1};
+        end
     end
-    isolatedVertices = sum(output, 1) == 0 & sum(output, 2)' == 0;
-    output = output(~isolatedVertices, ~isolatedVertices);
-    plot(digraph(output));
 end
